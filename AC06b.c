5e5c084d882be1a9beccb7fc2009edb5af51bea6
7chii <marianapacper@outlook.com>
2024-04-10 13:45:25 -0300

    Exercicios aula 10/04/24

--- AC06b.c
+++ AC06b.c
@@ -1,21 +1,35 @@
 #include <stdio.h>
 // fibonacci
 // recursivo
-void func(int a, int b, int cas, int num)
+long func(int num)
 {
-    if (cas > 0)
+    if (num == 0)
     {
-        int c = a + b;
-        printf("%d + %d = %d\n", a, b, c);
-        a = b;
-        b = c;
-        func(a, b, cas - 1, num);
+        return 0;
+    }
+    else if (num == 1)
+    {
+        return 1;
     }
     else
     {
-    }
+        return (func(num - 1) + func(num - 2));
+    };
 }
-// iterativo
+void funci(int cas)
+{
+    int a = 0;
+    int b = 1;
+    int c = 0;
+    for (int i = 0; i < cas; i++)
+    {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    printf("numero %d aparece!\n",a);
+}
+// fibonacci print aproximado
 void funciter(int a, int b, int cas)
 {
     for (int i = 0; i < cas; i++)
@@ -33,15 +47,17 @@ int main()
     b = 1;
     printf("quantas casas de fibonacci?\n:");
     scanf(" %d", &cas);
-    cas = cas - 1;
-    cas2 = cas;
     printf("recurs!\n");
-    func(a, b, cas, cas2);
+    cas = cas - 1;
+    cas2 = func(cas);
+    printf("numero %d aparece!\n", cas2);
     printf("iter!\n");
+    funci(cas);
+    printf("\n");
     funciter(a, b, cas);
 }
 /* 3-> analise de complexidade dos algoritmos)
-    alg1 e 2:sequencia n dado tamanho n retornar seq ao contrario 
+    alg1 e 2:sequencia n dado tamanho n retornar seq ao contrario
         o algoritmo executa n passos, sem diferenca
     alg 3 e 4:fibonacci
         recurs: chamadas internas, ex para n = 6 -> 16 vezes
